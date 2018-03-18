1035d9b1749c8e8131fe51b0fdf5a409db8d8d39
Harish Saini <harishsaini.lajak@gmail.com>
2018-03-18 21:24:26 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,14 +1,13 @@
-console.log('Loaded!');
+var button = document.getElementById('button');
 
-img = document.getElementById("madi");
-var marginLeft=0;
-var sign=1;
-function moveRight(){
-    marginLeft=marginLeft+(sign)*5;
-    if(marginLeft===1000||marginLeft===0)
-    sign*=-1;
-    img.style.marginLeft=marginLeft+"px";
+var counter=0;
+button.onclick= function(){
+    //Make request to counter
+    
+    //fetch result
+    
+    //display in the span
+    counter=counter+1;
+    var span= document.getElementById('count');
+    span.innerHTML = counter.toString();
 }
-img.onclick = function(){
-    var interval = setInterval(moveRight,50);
-};
