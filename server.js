8ca83a8cbb2e8898fa4311556fb5c622dd1fb142
Harish Saini <harishsaini.lajak@gmail.com>
2018-03-18 22:23:51 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -173,9 +173,8 @@ app.get('/counter',function(req,res){
    res.send(counter.toString());
 });
 
-var names=[];
-app.get('submit-name/:name',function(req,res){
-   res.send("made req");
+app.get('/submit',function(req,res){
+   res.send("harish");
 });
 
 app.get('/ui/main.js',function(req,res){
