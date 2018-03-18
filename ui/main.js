db44c5622318c757d2d5d3f471dd7a5efad3664e
Harish Saini <harishsaini.lajak@gmail.com>
2018-03-18 21:04:23 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,14 +1,5 @@
 console.log('Loaded!');
 
-var element = document.getElementById('text');
-element.innerHTML=`
-    <div>
-        <h1>Home Page</h1>
-        <p class="center" margin="auto">Welcome to the Home page. Content is changed by the javascript code.
-        </p>
-    </div>
-`;
-element.style.margin="auto";
 img = document.getElementById("madi");
 var marginLeft=0;
 var sign=1;
