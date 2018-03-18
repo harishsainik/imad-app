b6596e2d1d13be8e3690db121eb48d977badba3d
Harish Saini <harishsaini.lajak@gmail.com>
2018-03-18 11:54:46 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,7 +4,7 @@ var element = document.getElementById('text');
 element.innerHTML=`
     <div>
         <h1>Home Page</h1>
-        <p class="center">Welcome to the Home page. Content is changed by the javascript code.
+        <p class="center" margin="auto">Welcome to the Home page. Content is changed by the javascript code.
         </p>
     </div>
 `;
