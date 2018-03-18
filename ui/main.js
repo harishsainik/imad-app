84ca515df7ff3933a078829fa20f3b3eb1e4e96d
Harish Saini <harishsaini.lajak@gmail.com>
2018-03-18 11:51:15 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,10 @@
 console.log('Loaded!');
+
+var element = document.getElementById('text');
+element.innerHTML=`
+    <div>
+        <h1>Home Page</h1>
+        <p class="center text-big bold">Welcome to the Home page. Content is changed by the javascript code.
+        </p>
+    </div>
+`;
