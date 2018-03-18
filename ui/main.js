b647f26c6f3863180adc80afef1e275786f8ff9c
Harish Saini <harishsaini.lajak@gmail.com>
2018-03-18 21:33:59 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,12 +2,26 @@ var button = document.getElementById('counter');
 
 var counter=0;
 button.onclick= function(){
-    //Make request to counter
+    //Create the request object
+    var req = new XMLHttpRequest();
     
-    //fetch result
+    //define action to be taken on state change of request
+    req.onreadystatechange = function(){
+      if(req.readyState === XMLHttpRequest.DONE)
+      {
+          //if request if done successfully take action
+          if(req.status === 200)
+          {
+              var counter=req.responseText;
+              var span= document.getElementById('count');
+              span.innerHTML = counter.toString();
+          }
+      }
+      //Request not done yet
+    };
     
-    //display in the span
-    counter=counter+1;
-    var span= document.getElementById('count');
-    span.innerHTML = counter.toString();
-}
+    
+    //Make the request
+    req.open('GET','http://harishsainilajak.imad.hasura-app.io/counter',true);
+    req.send(null);
+};
