c8f5ca29e64e6ecc21fabbfb2a8347919d10b584
Harish Saini <harishsaini.lajak@gmail.com>
2018-03-21 14:51:01 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -113,7 +113,6 @@ app.get('/ui/article3.js',function(req,res){
 });
 
 app.get('/articles/:articleName',function(req,res){
-  var articleName=req.params.articleName;
   pool.query("SELECT * from article where title=$1",[req.params.articleName],function(err,result){
       if(err){
           res.status(500).send(err.toString());
