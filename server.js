aefab5451018bd358f873fe57e1c5880ce2c9420
Harish Saini <harishsaini.lajak@gmail.com>
2018-03-22 11:29:18 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -59,7 +59,7 @@ app.get('/', function (req, res) {
 function hash(input, salt)
 {
     var hashed = crypto.pbkdf2Sync(input, salt, 10000, 512, 'sha512');
-    return hashed.toString('hex');
+    return ['pbkdf2',salt,10000,hashed.toString('hex')].join('$');
 }
 app.get('/hash/:input',function(req,res){
    var hashString = hash(req.params.input,'This is salt string');
