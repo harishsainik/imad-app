7c60d0442ebd1ce624b6bb8cbb21d0485a97de49
Harish Saini <harishsaini.lajak@gmail.com>
2018-04-16 23:01:33 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,4 @@
+//For login
 //Submit username and password
 var submit = document.getElementById('submit_button');
 
@@ -34,6 +35,45 @@ submit.onclick = function(){
     console.log(password);
     //Make the request
     req.open('POST','http://harishsainilajak.imad.hasura-app.io/login',true);
+    req.setRequestHeader('Content-Type','application/json');
+    req.send(JSON.stringify({username:username,password:password}));
+    
+};
+
+//For Registration
+//Submit username and password
+var submit = document.getElementById('register_button');
+
+submit.onclick = function(){
+     //Create the request object
+    var req = new XMLHttpRequest();
+    
+    //define action to be taken on state change of request
+    req.onreadystatechange = function(){
+      if(req.readyState === XMLHttpRequest.DONE)
+      {
+          //if request if done successfully take action
+          if(req.status === 200)
+          {
+              console.log("user created");
+              alert("Sign up successful");
+              submit.innerHtml="Registered";
+          }
+          else if(req.status === 500){
+              alert("Internal server error");
+          }
+          
+      }
+      //Request not done yet
+    };
+    
+    //fetch username and password
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+    console.log(username);
+    console.log(password);
+    //Make the request
+    req.open('POST','http://harishsainilajak.imad.hasura-app.io/create-user',true);
     req.setRequestHeader('Content-Type','application/json');
     req.send(JSON.stringify({username:username,password:password}));
     
